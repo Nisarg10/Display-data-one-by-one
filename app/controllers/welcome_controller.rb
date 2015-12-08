3683e505bd3e479e5bb0bba04d3fe66e2ad0573f
Kishan Bhavesh Vekaria <Kishan@Kishans-MacBook-Pro.local>
2015-12-07 23:18:05 -0600

    Demo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,15 @@
-class WelcomeController < ApplicationController
+require "mongo"
+include Mongo
 
+class WelcomeController < ApplicationController
+  	
   # GET /welcome
   def abc
 
 
   	id = params[:id]
 
-  	require 'mongo'
+
 	connection = Mongo::Client.new(['ds059654.mongolab.com:59654/heroku_vknx18tr'], :database => 'heroku_vknx18tr', :user => 'nsd6275', :password => 'nsd6275')
 
 	connection[:DeathCause].find('id' => id).each {|data| 
