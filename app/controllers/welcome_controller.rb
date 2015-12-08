f3a10f2e5fb8d3b1c966a639fbee2203370c3e29
Kishan Bhavesh Vekaria <Kishan@Kishans-MacBook-Pro.local>
2015-12-08 17:18:44 -0600

    Demo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,12 +9,12 @@ class WelcomeController < ApplicationController
 
 	connection = Mongo::Client.new(['ds059654.mongolab.com:59654/heroku_vknx18tr'], :database => 'heroku_vknx18tr', :user => 'nsd6275', :password => 'nsd6275');
 
-	@data = connection.database.collection('DeathCause').find[:id => params[:id]]
+	@data = connection.database.collection('DeathCause').find({:id => params[:id]});
 	
 end
 	
   render json: @data
 
-  end
+ 
 
 end
