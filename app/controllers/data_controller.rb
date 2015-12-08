34a055eae2efbbe69fac40fa0967af710523985d
Kishan Bhavesh Vekaria <Kishan@Kishans-MacBook-Pro.local>
2015-12-08 17:59:01 -0600

    Demo

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -4,8 +4,8 @@ require 'json'
 class DataController < ApplicationController
 
 	def fetch
-		
-		
+
+
 		connection = Mongo::Client.new(['ds059654.mongolab.com:59654/heroku_vknx18tr'], :database => 'heroku_vknx18tr', :user => 'nsd6275', :password => 'nsd6275');
 
 		data = connection.database
@@ -15,4 +15,5 @@ class DataController < ApplicationController
 	end
 
 	render json: @db
+end
 end
