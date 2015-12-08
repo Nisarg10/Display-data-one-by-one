32f8fb7ad3c34e79b35417872b9db1ff2d00ab59
Kishan Bhavesh Vekaria <Kishan@Kishans-MacBook-Pro.local>
2015-12-07 21:35:45 -0600

    Demo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,11 @@ class WelcomeController < ApplicationController
   # GET /welcome
   def index
 
-  	id = params[:id]
-
   	require "mongo"
 	include Mongo
+	
+  	id = params[:id]
+
 
 	connection = Mongo::Client.new(['ds059654.mongolab.com:59654/heroku_vknx18tr'], :database => 'heroku_vknx18tr', :user => 'nsd6275', :password => 'nsd6275')
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  get controllers/welcome_controller
+  get 'welcome/index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
