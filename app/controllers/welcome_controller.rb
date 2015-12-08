237b382017bb2913462475c5e490012b615f66eb
Kishan Bhavesh Vekaria <Kishan@Kishans-MacBook-Pro.local>
2015-12-08 16:45:59 -0600

    DEmo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,15 +6,13 @@ class WelcomeController < ApplicationController
   # GET /welcome
   def abc
 
-  id = params[:id]
 
+	connection = Mongo::Client.new(['ds059654.mongolab.com:59654/heroku_vknx18tr'], :database => 'heroku_vknx18tr', :user => 'nsd6275', :password => 'nsd6275');
 
-	connection = Mongo::Client.new(['ds059654.mongolab.com:59654/heroku_vknx18tr'], :database => 'heroku_vknx18tr', :user => 'nsd6275', :password => 'nsd6275')
-
-	@data = connection[:DeathCause].find('id' => params[:id])
+	@data = connection[:DeathCause].find('id' => params[:id]);
 	
 
-	render json:  @data
+	render json: @data
 
   end
 
