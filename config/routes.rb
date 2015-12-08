6bb01c280630ed7c360e925b8f48116c93eea9c6
Kishan Bhavesh Vekaria <Kishan@Kishans-MacBook-Pro.local>
2015-12-07 21:43:58 -0600

    Demo

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  get 'WelcomeController/index'
+  get 'welcome_controller/index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
