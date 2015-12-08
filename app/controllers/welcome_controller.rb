aa3778c3c99bb96b68d3d97af6d8994df513ff8d
Kishan Bhavesh Vekaria <Kishan@Kishans-MacBook-Pro.local>
2015-12-08 14:17:18 -0600

    Demo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,13 +7,13 @@ class WelcomeController < ApplicationController
   	require "mongo"
   	include Mongo
 
-  	id = params[:id]
+  	id = 1
 
 
 	connection = Mongo::Client.new(['ds059654.mongolab.com:59654/heroku_vknx18tr'], :database => 'heroku_vknx18tr', :user => 'nsd6275', :password => 'nsd6275')
 
 	connection[:DeathCause].find('id' => id).each {|data| 
-	}
+	puts data}
 
 	render :json => data
 
